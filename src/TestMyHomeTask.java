f600bb4df9184ac73a86fc0a8a4a96624cd50dd7
MoshkaBortmam <TryagenButor.ru>
2020-04-08 03:12:31 +0300

    rename varrible

--- src/TestMyHomeTask.java
+++ src/TestMyHomeTask.java
@@ -9,24 +9,24 @@ public class TestMyHomeTask {
         obstacles[0] = new Wall(2);
         obstacles[1] = new Track(500);
         obstacles[2] = new Track(50000);
-        ArrayList<RunblJumpbl> rubJumpbl = new ArrayList<>();
-        rubJumpbl.add(new Cat("Барсик", 100, 3));
-        rubJumpbl.add(new Human("Мошка", 5000, 2));
-        rubJumpbl.add(new Robot("II", 1000000, 10));
-        rubJumpbl.add(new Cat("Сик", 500, 5));
-        rubJumpbl.add(new Human("Мышка", 1000, 1));
-        rubJumpbl.add(new Robot("II1", 10, 1));
-        rubJumpbl.add(new Robot("II2", 1000, 2));
+        ArrayList<RunblJumpbl> runJumpbl = new ArrayList<>();
+        runJumpbl.add(new Cat("Барсик", 100, 3));
+        runJumpbl.add(new Human("Мошка", 5000, 2));
+        runJumpbl.add(new Robot("II", 1000000, 10));
+        runJumpbl.add(new Cat("Сик", 500, 5));
+        runJumpbl.add(new Human("Мышка", 1000, 1));
+        runJumpbl.add(new Robot("II1", 10, 1));
+        runJumpbl.add(new Robot("II2", 1000, 2));
 
 
         for (Obstacles obs : obstacles) {
             System.out.println("Начало препятствия" + System.lineSeparator());
-            for (int i = 0; i < rubJumpbl.size(); i++) {
-                if (obs.isOut(rubJumpbl.get(i))) {
-                    rubJumpbl.remove(rubJumpbl.get(i));
+            for (int i = 0; i < runJumpbl.size(); i++) {
+                if (obs.isOut(runJumpbl.get(i))) {
+                    runJumpbl.remove(runJumpbl.get(i));
                     --i;
                 } else {
-                    System.out.println(rubJumpbl.get(i).getName() + " прошел препятствие");
+                    System.out.println(runJumpbl.get(i).getName() + " прошел препятствие");
                 }
             }
         }
